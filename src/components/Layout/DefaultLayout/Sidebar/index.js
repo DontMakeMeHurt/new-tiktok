39cf8c3daacbb4778ea01dd767ae67b4d2577587
hoang anh trung <hoanganhtrung1132@gmail.com>
2022-09-13 02:12:08 +0700

    [add-css/default-layout]

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,8 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function SideBar() {
     return (
-        <div>
+        <aside className={cx('wrapper')}>
             <h2>SideBar</h2>
-        </div>
+        </aside>
     );
 }
 
