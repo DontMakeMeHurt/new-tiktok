bfc70fd1d836584c68bd55b54ad4fc8a17001ad6
hoang anh trung <hoanganhtrung1132@gmail.com>
2022-12-14 01:55:42 +0700

    add logo tiktok

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
@@ -7,8 +8,15 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* logo */}
-                {/*  */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and video" spellCheck="false" />
+                    <button>{/* icon close */}</button>
+                    {/* loading */}
+                    <div className={cx('search-btn')}>{/* icon search */}</div>
+                </div>
             </div>
         </header>
     );
